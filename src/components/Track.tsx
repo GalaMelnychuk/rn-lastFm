94685495b1089e3260f058fb19735bd94c8893af
Gala <galinas.vm@gmail.com>
2020-10-28 09:53:28 +0200

    Revert "added useEffect on HomeScreen and added render items"
    This reverts commit d91d4a9af8f2b5dd9bdd0f4c5978828727727ee3.

--- src/components/Track.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-import React from 'react';
-import { View } from 'react-native';
-
-export const Track = ({ name }) => {
-  return <Text>{name}</Text>;
-};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,25 +1,12 @@
 import React, { FC, useEffect, useRef } from 'react';
 import { Text } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { getPopularTracksAction } from '../redux/actions/popularTracksActions';
 import { requests } from '../services/requests';
-import { Track } from '../components/Track';
 
 export const HomeScreen = () => {
   const dispatch = useDispatch();
-  const tracks = useSelector((state) => state.tracks);
 
-  useEffect(() => {
-    dispatch(getPopularTracksAction());
-  }, []);
-
-  return (
-    <FlatList
-      data={tracks}
-      renderItem={({ item }) => <Track name={item.name} />}
-      keyExtractor={(item) => item.name}
-    />
-  );
+  return <Text onPress={() => dispatch(getPopularTracksAction())}>Home</Text>;
 };
