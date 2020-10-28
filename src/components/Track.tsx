d91d4a9af8f2b5dd9bdd0f4c5978828727727ee3
Gala <galinas.vm@gmail.com>
2020-10-28 02:03:57 +0200

    added useEffect on HomeScreen and added render items

---
+++ src/components/Track.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+
+export const Track = ({ name }) => {
+  return <Text>{name}</Text>;
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,25 @@
 import React, { FC, useEffect, useRef } from 'react';
 import { Text } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { FlatList } from 'react-native-gesture-handler';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getPopularTracksAction } from '../redux/actions/popularTracksActions';
 import { requests } from '../services/requests';
+import { Track } from '../components/Track';
 
 export const HomeScreen = () => {
   const dispatch = useDispatch();
+  const tracks = useSelector((state) => state.tracks);
 
-  return <Text onPress={() => dispatch(getPopularTracksAction())}>Home</Text>;
+  useEffect(() => {
+    dispatch(getPopularTracksAction());
+  }, []);
+
+  return (
+    <FlatList
+      data={tracks}
+      renderItem={({ item }) => <Track name={item.name} />}
+      keyExtractor={(item) => item.name}
+    />
+  );
 };
